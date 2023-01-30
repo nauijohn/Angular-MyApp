b2ff998ea512683e9dcd0f6683e11cf6d412fc22
jhnauiabsi <85679013+nauijohn@users.noreply.github.com>
2023-01-30 23:11:45 +0800

    Anthony-PC
    Continued from Anthony-PC

--- MyApp/src/app/recipes/recipe-list/recipe-list.component.ts
+++ MyApp/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -12,7 +12,12 @@ export class RecipeListComponent implements OnInit {
 		new Recipe(
 			'A Test Recipe',
 			'This is simply a test',
-			'https://www.google.com/imgres?imgurl=https%3A%2F%2Flh3.googleusercontent.com%2F3MTaO0OfMwrDKcuhwDjHzKKdO-_daDhCPCqmFfSZSkQC5oXq60Ogpd6I57zjE-ZSsCrom1Wrw4i305jccSlbNXMQ1oaI7AVvMA%3Dw1667-h1667-c-rj-v1-e365&imgrefurl=https%3A%2F%2Fwww.yummly.com%2Frecipes&tbnid=y8WNo4nQ1woZvM&vet=12ahUKEwiHz9nSiO_8AhWJBaYKHXpNDzsQMyghegUIARDUAg..i&docid=vTLa09D8U97lwM&w=1667&h=1667&q=recipe&ved=2ahUKEwiHz9nSiO_8AhWJBaYKHXpNDzsQMyghegUIARDUAg'
+			'https://i0.wp.com/www.onceuponachef.com/images/2020/05/best-grilled-chicken-scaled.jpg?resize=1120%2C1547&ssl=1'
+		),
+		new Recipe(
+			'A Test Recipe',
+			'This is simply a test',
+			'https://i0.wp.com/www.onceuponachef.com/images/2020/05/best-grilled-chicken-scaled.jpg?resize=1120%2C1547&ssl=1'
 		),
 	];
 
